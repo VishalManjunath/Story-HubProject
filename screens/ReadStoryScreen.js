682e7902b76b2017183d8d8929de703b370d3317
VishalManjunath <vishal15.dk@gmail.com>
2021-08-28 10:08:48 +0530

    first commit

---
+++ screens/ReadStoryScreen.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import {Text, View, TouchableOpacity} from 'react-native';
+
+export default class ReadStory extends React.Component {
+    render() {
+        return(
+            <Text>Read Story</Text>
+        );
+    }
+}
--- screens/WriteStoryScreen.js
+++ screens/WriteStoryScreen.js
@@ -1,2 +1,27 @@
 import * as React from 'react';
-import {Text, View} from 'react'
+import {Text, View, TouchableOpacity} from 'react-native';
+import {Header} from 'react-native-elements';
+import {TextInput} from 'react-native-gesture-handler';
+
+export default class WriteStory extends React.Component {
+    render() {
+        return(
+            <View>
+                <Header />
+                <TextInput
+                placeHolder = "StoryTitle"
+                />
+                <TextInput
+                placeHolder = "Author"
+                />
+                <TextInput
+                multiline = {true}
+                placeHolder = "WriteStory"
+                />
+                <TouchableOpacity>
+                    <Text>Submit</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
